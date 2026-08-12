ecdc99a6a552c0fff89c98c97b72b97fa3072853
Selleryd <178099989+Selleryd@users.noreply.github.com>
2026-08-12 08:31:40 -0400

    Delete malach-public-website directory

--- malach-public-website/api/health.js
+++
@@ -1,5 +0,0 @@
-import { sendJson, methodNotAllowed } from './_lib/http.js';
-export default async function handler(req, res) {
-  if (req.method !== 'GET') return methodNotAllowed(res, ['GET']);
-  sendJson(res, 200, { ok: true, service: 'malach-public-site', timestamp: new Date().toISOString() });
-}
